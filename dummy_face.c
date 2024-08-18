7dba152ddf484c7d63dd2b48f62381a2f313b111
Dennis Faucher <dennis@faucher.net>
2024-08-18 15:28:21 -0400

    Add files via upload

--- dummy_face.c
+++ dummy_face.c
@@ -22,6 +22,7 @@
  * SOFTWARE.
  */
 
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "dummy_face.h"
@@ -30,6 +31,94 @@
 int lowTideMinutes = 722; //Changed to a global variable
 int lowTideHourGap = 0;
 int resetTide = 0; // change for compile errors to fix
+uint32_t currentDummyTime;
+uint32_t lastDummyTime;
+watch_date_time dummyNow;
+uint32_t dummyDeltaMinutes = 722;
+//int dummyAlarmPressed = 0;
+
+// Function taken from `src/time/__year_to_secs.c` of musl libc
+// https://musl.libc.org
+static uint32_t __dummy_year_to_secs(uint32_t year, int *is_leap)
+{
+	if (year-2ULL <= 136) {
+		int y = year;
+		int leaps = (y-68)>>2;
+		if (!((y-68)&3)) {
+			leaps--;
+			if (is_leap) *is_leap = 1;
+		} else if (is_leap) *is_leap = 0;
+		return 31536000*(y-70) + 86400*leaps;
+	}
+
+	int cycles, centuries, leaps, rem;
+
+	if (!is_leap) is_leap = &(int){0};
+	cycles = (year-100) / 400;
+	rem = (year-100) % 400;
+	if (rem < 0) {
+		cycles--;
+		rem += 400;
+	}
+	if (!rem) {
+		*is_leap = 1;
+		centuries = 0;
+		leaps = 0;
+	} else {
+		if (rem >= 200) {
+			if (rem >= 300) centuries = 3, rem -= 300;
+			else centuries = 2, rem -= 200;
+		} else {
+			if (rem >= 100) centuries = 1, rem -= 100;
+			else centuries = 0;
+		}
+		if (!rem) {
+			*is_leap = 0;
+			leaps = 0;
+		} else {
+			leaps = rem / 4U;
+			rem %= 4U;
+			*is_leap = !rem;
+		}
+	}
+
+	leaps += 97*cycles + 24*centuries - *is_leap;
+
+	return (year-100) * 31536000LL + leaps * 86400LL + 946684800 + 86400;
+}
+
+// Function taken from `src/time/__month_to_secs.c` of musl libc
+// https://musl.libc.org
+static int __dummy_month_to_secs(int month, int is_leap)
+{
+	static const int secs_through_month[] = {
+		0, 31*86400, 59*86400, 90*86400,
+		120*86400, 151*86400, 181*86400, 212*86400,
+		243*86400, 273*86400, 304*86400, 334*86400 };
+	int t = secs_through_month[month];
+	if (is_leap && month >= 2) t+=86400;
+	return t;
+}
+
+// Function adapted from `src/time/__tm_to_secs.c` of musl libc
+// https://musl.libc.org
+uint32_t __dummy_utility_convert_to_unix_time(uint16_t year, uint8_t month, uint8_t day, uint8_t hour, uint8_t minute, uint8_t second, uint32_t utc_offset) {
+    int is_leap;
+
+    // POSIX tm struct starts year at 1900 and month at 0
+    // https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/time.h.html
+    uint32_t timestamp = __dummy_year_to_secs(year - 1900, &is_leap);
+    timestamp += __dummy_month_to_secs(month - 1, is_leap);
+
+    // Regular conversion from musl libc
+    timestamp += (day - 1) * 86400;
+    timestamp += hour * 3600;
+    timestamp += minute * 60;
+    timestamp += second;
+    timestamp -= utc_offset;
+
+    return timestamp;
+}
 
 void dummy_face_setup(movement_settings_t *settings, uint8_t watch_face_index, void ** context_ptr) {
     (void) settings;
@@ -38,6 +127,8 @@ void dummy_face_setup(movement_settings_t *settings, uint8_t watch_face_index, v
         *context_ptr = malloc(sizeof(dummy_state_t));
         memset(*context_ptr, 0, sizeof(dummy_state_t));
         // Do any one-time tasks in here; the inside of this conditional happens only at boot.
+        dummyNow = watch_rtc_get_date_time();
+        lastDummyTime = __dummy_utility_convert_to_unix_time(dummyNow.unit.year, dummyNow.unit.month, dummyNow.unit.day, dummyNow.unit.hour, dummyNow.unit.minute, 0, -4);
     }
     // Do any pin or peripheral setup here; this will be called whenever the watch wakes from deep sleep.
 }
@@ -49,111 +140,17 @@ void dummy_face_activate(movement_settings_t *settings, void *context) {
     // Handle any tasks related to your watch face coming on screen.
 }
 
-// Function taken from `src/time/__year_to_secs.c` of musl libc
-// https://musl.libc.org
-//static uint32_t __year_to_secs(uint32_t year, int *is_leap)
-//{
-//	if (year-2ULL <= 136) {
-//		int y = year;
-//		int leaps = (y-68)>>2;
-//		if (!((y-68)&3)) {
-//			leaps--;
-//			if (is_leap) *is_leap = 1;
-//		} else if (is_leap) *is_leap = 0;
-//		return 31536000*(y-70) + 86400*leaps;
-//	}
-//
-//	int cycles, centuries, leaps, rem;
-//
-//	if (!is_leap) is_leap = &(int){0};
-//	cycles = (year-100) / 400;
-//	rem = (year-100) % 400;
-//	if (rem < 0) {
-//		cycles--;
-//		rem += 400;
-//	}
-//	if (!rem) {
-//		*is_leap = 1;
-//		centuries = 0;
-//		leaps = 0;
-//	} else {
-//		if (rem >= 200) {
-//			if (rem >= 300) centuries = 3, rem -= 300;
-//			else centuries = 2, rem -= 200;
-//		} else {
-//			if (rem >= 100) centuries = 1, rem -= 100;
-//			else centuries = 0;
-//		}
-//		if (!rem) {
-//			*is_leap = 0;
-//			leaps = 0;
-//		} else {
-//			leaps = rem / 4U;
-//			rem %= 4U;
-//			*is_leap = !rem;
-//		}
-//	}
-//
-//	leaps += 97*cycles + 24*centuries - *is_leap;
-//
-//	return (year-100) * 31536000LL + leaps * 86400LL + 946684800 + 86400;
-//}
-
-// Function taken from `src/time/__month_to_secs.c` of musl libc
-// https://musl.libc.org
-//static int __month_to_secs(int month, int is_leap)
-//{
-//	static const int secs_through_month[] = {
-//		0, 31*86400, 59*86400, 90*86400,
-//		120*86400, 151*86400, 181*86400, 212*86400,
-//		243*86400, 273*86400, 304*86400, 334*86400 };
-//	int t = secs_through_month[month];
-//	if (is_leap && month >= 2) t+=86400;
-//	return t;
-//}
-// Function adapted from `src/time/__tm_to_secs.c` of musl libc
-// https://musl.libc.org
-//uint32_t __watch_utility_convert_to_unix_time(uint16_t year, uint8_t month, uint8_t day, uint8_t hour, uint8_t minute, uint8_t second, uint32_t utc_offset) {
-//    int is_leap;
-//
-//    // POSIX tm struct starts year at 1900 and month at 0
-//    // https://pubs.opengroup.org/onlinepubs/9699919799/basedefs/time.h.html
-//    uint32_t timestamp = __year_to_secs(year - 1900, &is_leap);
-//    timestamp += __month_to_secs(month - 1, is_leap);
-//
-//    // Regular conversion from musl libc
-//    timestamp += (day - 1) * 86400;
-//    timestamp += hour * 3600;
-//    timestamp += minute * 60;
-//    timestamp += second;
-//    timestamp -= utc_offset;
-//
-//    return timestamp;
-//}
-
 bool dummy_face_loop(movement_event_t event, movement_settings_t *settings, void *context) {
     //dummy_state_t *state = (dummy_state_t *)context;
     watch_date_time date_time;
     int nowSeconds;
-    //int modSeconds;
-    //int lowTideMinutes;
-    //if(lowTideMinutes < 0 || lowTideMinutes > 722){
-    //    lowTideMinutes = 722;
-    //}
-    //int lowTideMinutes = 722;
-    //int lowTideHourGap = 0;
-    //int NumTidesRemainder;
-    //char buf[7];
-    //char tideString[7];
-    //char modHour[3];
-    char eightString[9];
-    //uint32_t realLowTideTime;
-    //uint32_t currentUNIXTime;
-    //uint32_t differenceUNIXTime;
-
+    char eightString[8+1];
     switch (event.event_type) {
         case EVENT_ACTIVATE:
             // Show your initial UI here.
+            char tenString[10+1];
+            sprintf(tenString, "    graph ");
+            watch_display_string(tenString, 0);
              //lowTideMinutes = 722;
              //sprintf(eightString, "%02d%s", 0, "llllll");
              //watch_display_string(eightString, 2);
@@ -162,37 +159,15 @@ bool dummy_face_loop(movement_event_t event, movement_settings_t *settings, void
             // If needed, update your display here.
             date_time = watch_rtc_get_date_time();
             nowSeconds = date_time.unit.second;
-            //modSeconds = nowSeconds % 12;   //Actual mod (%) will be based on 12 hours and 2 minutes = 722 minutes
-                                            // Maybe base on elapsed UNIX time
-            // UNIX Time Test
-            // uint32_t watch_utility_convert_to_unix_time(uint16_t year, uint8_t month, uint8_t day, uint8_t hour, uint8_t minute,
-            // uint8_t second, uint32_t utc_offset)
-            //
-            //realLowTideTime = __watch_utility_convert_to_unix_time(2024, 8, 3, 27, 0, 0, -4); // Find the UNIX time of an actual previous low tide
-                                                                                            // UNIX = 1722540420
-            //currentUNIXTime = __watch_utility_convert_to_unix_time(date_time.unit.year, date_time.unit.month, date_time.unit.day, date_time.unit.hour, date_time.unit.minute, 0, -4);
-                                                                                            // UNIX = 1722822780 8/4/2024 9:53 PM
-            //differenceUNIXTime = currentUNIXTime - realLowTideTime; // UNIX Time is number of seconds since 1/1/1970, so this is seconds elapsed
-                                                                    // 1722822780 - 1722540420 = 282360
-            // Low tide every 722 minutes = 43320 seconds. Can we MOD 43320?
-            
-            //if(resetTide == 1){ // Not sure if this if statement is actually needed. Might be a global variable hack.
-            //    lowTideMinutes = 722;
-            //    resetTide = 0;
+            currentDummyTime = __dummy_utility_convert_to_unix_time(date_time.unit.year, date_time.unit.month, date_time.unit.day, date_time.unit.hour, date_time.unit.minute, 0, -4);
+            dummyDeltaMinutes = (currentDummyTime - lastDummyTime)/60;
+            lastDummyTime = currentDummyTime;
+            lowTideMinutes = lowTideMinutes - dummyDeltaMinutes;
+            lowTideHourGap = (lowTideMinutes / 60.166);
+            //if(nowSeconds==0){ 				// Change tide once every 60.166 minutes (722/12)
+            //    lowTideMinutes = lowTideMinutes - 60;
+            //    lowTideHourGap = (lowTideMinutes / 60.166);
             //}
-
-            if(nowSeconds==0){ 				// Change tide once every 60.166 minutes (722/12)
-                lowTideMinutes = lowTideMinutes - 1;
-                lowTideHourGap = (lowTideMinutes / 60.166);
-            }
-
-            //NumTidesRemainder = (differenceUNIXTime % 43320)/60/60; // Find the remainder after dividing by tide cycle seconds
-                                                            // 43320 is seconds so / 60 /60 to get hours
-                                                            // 282360 % 43320 is  quotient:6 remainder:22440
-                                                            // 22440/60/60 = 6.23. It is actually 8 hours and 40 minutes to the next low tide
-                                                            // There actually HAVE been 6 low tides since then. The remainder is what we want
-                                                            // 0.23 * 43320 = 9963.6 seconds 9963.6/60/60 = 2.76. That is not 8 hours.
-                                                            // 1.00-0.23=0.77. 0.77*43320 = 33356.4 seconds. 33356.4/60/60=9.26. Also not 8 hourds
 
             switch (lowTideHourGap) {
                 case 0:
@@ -237,28 +212,26 @@ bool dummy_face_loop(movement_event_t event, movement_settings_t *settings, void
                 default:
                     strcpy(eightString, "????????");
             }
-            //sprintf(eightString, "%02d%06d", lowTideHourGap, lowTideMinutes);
             // 4th position is the first position of the large characters
             watch_display_string(eightString, 2);
             break;
 
          case EVENT_ALARM_LONG_PRESS: // Use a long press to reset lowTideMinutes to 722 minutes (low tide)
-             //lowTideMinutes = 722;
-             //lowTideHourGap = (lowTideMinutes / 60.166);
-             //sprintf(eightString, "%02d%s", lowTideHourGap, "llllll");
-             //watch_display_string(eightString, 2);
-             //resetTide = 1;
              break;
         case EVENT_ALARM_BUTTON_UP: // Use quick press of alarm to adjust the tide one segment (~ 1hour)
-            if (lowTideHourGap == 0){
-               lowTideHourGap = 11;
-               lowTideMinutes = lowTideHourGap * 60.166;
-            } else  {
-               lowTideHourGap = lowTideHourGap - 1;
-               lowTideMinutes = lowTideHourGap * 60.166;
+            if (lowTideMinutes <= 60){
+                lowTideMinutes = 722;
             }
-             //sprintf(eightString, "%02d%s", lowTideHourGap, "888888");
-             //watch_display_string(eightString, 2);
+            else{
+                lowTideMinutes = lowTideMinutes - 60;                
+            }
+            //if (lowTideHourGap == 0){
+            //   lowTideHourGap = 11;
+            //   lowTideMinutes = lowTideHourGap * 60.166;
+            //} else  {
+            //   lowTideHourGap = lowTideHourGap - 1;
+            //   lowTideMinutes = lowTideHourGap * 60.166;
+            //}
             break;
 
         case EVENT_LIGHT_BUTTON_UP:
